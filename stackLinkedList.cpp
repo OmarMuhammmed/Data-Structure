232364ca2456d745f7bb2074277bac5d70631962
OmarMuhammmed <amorey2006@gmail.com>
2024-12-04 06:59:28 +0200

    stack, queue Linked list

--- stackLinkedList.cpp
+++ stackLinkedList.cpp
@@ -11,68 +11,67 @@ public:
     }
 };
 
-
 class Stack {
-
-    // head of the linked list
-    Node* head;
+   
+    Node* top;
 
 public:
-    // Constructor to initialize the stack
-    Stack() { this->head = nullptr; }
+    
+    Stack() { this->top = nullptr; }
 
-    // Function to check if the stack is empty
-    bool isEmpty() {
-      
-        // If head is nullptr, the stack is empty
-        return head == nullptr;
+    
+    bool isEmpty() 
+    {   
+        return top == nullptr;
     }
 
     // Function to push an element onto the stack
-    void push(int new_data) {
+    void push(int data) 
+    {
       
         // Create a new node with given data
-        Node* new_node = new Node(new_data);
+        Node* new_node = new Node(data);
 
         // Check if memory allocation for the new node
-        // failed
         if (!new_node) {
             cout << "\nStack Overflow";
         }
 
         // Link the new node to the current top node
-        new_node->next = head;
+        new_node->next = top;
 
         // Update the top to the new node
-        head = new_node;
+        top = new_node;
     }
 
     // Function to remove the top element from the stack
-    void pop() {
+    void pop() 
+    {
 
-        // Check for stack underflow
+       
         if (this->isEmpty()) {
             cout << "\nStack Underflow" << endl;
         }
         else {
+            
             // Assign the current top to a temporary
-            // variable
-            Node* temp = head;
+            Node* del = top;
 
             // Update the top to the next node
-            head = head->next;
+            top = top->next;
 
             // Deallocate the memory of the old top node
-            delete temp;
+            delete del;
         }
     }
 
     // Function to return the top element of the stack
-    int peek() {
+    int peek() 
+    {
 
         // If stack is not empty, return the top element
         if (!isEmpty())
-            return head->data;
+            return top->data;
         else {
             cout << "\nStack is empty";
             return INT_MIN;
